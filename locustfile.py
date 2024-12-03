1f75b102b15c412a9932bfa50b415af787a136a7
mert <mertsay@hotmail.de>
2024-12-03 15:50:33 +0100

    test

--- locustfile.py
+++ locustfile.py
@@ -3,7 +3,3 @@ from locust import HttpUser, task, between
 class MyUser(HttpUser):
     host = "https://demo.iqsuite.com"
     wait_time = between(1, 5)  # Wartezeit zwischen Anfragen (in Sekunden)
-
-    @task
-    def load_home_page(self):
-        self.client.get("/api/gbs/monitor/quarantines/available")  
