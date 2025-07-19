913b1b906343681c5ad26277334c7bff66fb0cce
Satish Janghel <satishjanghel@tranetechnologies.com>
2025-07-19 19:29:33 +0530

    Learn Jenkins for Trane

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Jenkins
+          Learn Jenkins for Trane
         </a>
       </header>
       <p>
-          Application version: 1
+          Application version: 1.1
       </p>
     </div>
   );
